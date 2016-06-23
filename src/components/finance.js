dfd11c0d6bbff3d1021f73bb973f76e86a96113a
linoor <michal.pomaranski@gmail.com>
2016-06-23 18:31:11 +0200

    before highcharts react

--- src/components/finance.js
+++ src/components/finance.js
@@ -1,6 +1,75 @@
 import React from 'react';
+var Highcharts = require('highcharts/highstock');
+window.Highcharts = Highcharts;
+require('highcharts/modules/exporting')(Highcharts);
+
 
 let apiKey = 'aGYvbFTWY2mshdTiXDkj9iMe40Zlp1cPS6bjsn1PhRVzEBHpuL';
+
+let getOptions = function (data) {
+    return {
+        title: {
+            text: 'AAPL stock price by minute'
+        },
+
+        subtitle: {
+            text: 'Using explicit breaks for nights and weekends'
+        },
+
+        xAxis: {
+            breaks: [{ // Nights
+                from: Date.UTC(2011, 9, 6, 16),
+                to: Date.UTC(2011, 9, 7, 8),
+                repeat: 24 * 36e5
+            }, { // Weekends
+                from: Date.UTC(2011, 9, 7, 16),
+                to: Date.UTC(2011, 9, 10, 8),
+                repeat: 7 * 24 * 36e5
+            }]
+        },
+
+        rangeSelector: {
+            buttons: [{
+                type: 'hour',
+                count: 1,
+                text: '1h'
+            }, {
+                type: 'day',
+                count: 1,
+                text: '1D'
+            }, {
+                type: 'all',
+                count: 1,
+                text: 'All'
+            }],
+            selected: 1,
+            inputEnabled: false
+        },
+
+        series: [{
+            name: 'AAPL',
+            type: 'area',
+            data: data,
+            gapSize: 5,
+            tooltip: {
+                valueDecimals: 2
+            },
+            fillColor: {
+                linearGradient: {
+                    x1: 0,
+                    y1: 0,
+                    x2: 0,
+                    y2: 1
+                },
+                stops: [
+                    [0, Highcharts.getOptions().colors[0]],
+                    [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                ]
+            },
+            threshold: null
+        }]
+    };
+};
 
 export default React.createClass({
   render() {
@@ -76,6 +145,8 @@ let CurrencyChanger = React.createClass({
         let firstOptions = this.state.currencies.filter(s => s !== this.state.secondSelect);
         let secondOptions = this.state.currencies.filter(s => s !== this.state.firstSelect);
 
+        const options = getOptions(this.state.data);
+
         return (
             <div className="">
                 <div onChange={this.handleChange} className="form-group">
@@ -105,6 +176,10 @@ let CurrencyChanger = React.createClass({
                         <div className="col-xs-8 col-xs-offset-2 text-center">
                             <Results result={result} />
                         </div>
+                    </div>
+
+                    <div className="row">
+                        <Chart container="container"/>
                     </div>
                 </div>
             </div>
@@ -170,5 +245,24 @@ let Reverser = React.createClass({
                 <span id="reverser" onClick={this.props.onClick} className="glyphicon glyphicon-refresh"/>
             </div>
         )
+    }
+})
+
+const Chart = React.createClass({
+    componentDidMount() {
+        $.getJSON('https://www.highcharts.com/samples/data/jsonp.php?filename=new-intraday.json&callback=?', (data) => {
+            this.chart = new Highcharts[this.props.type || "Chart"](
+                this.props.container,
+                data
+            )
+        });
+    },
+
+    componentWillUnmount() {
+        this.chart.destroy();
+    },
+
+    render() {
+        return <div id={this.props.container}></div>
     }
 })
